ea5f1c629404de9626859b1a8487efd7bb6ef4b7
rezgauche <94529557+rezgauche@users.noreply.github.com>
2022-01-25 22:59:42 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,34 +10,162 @@ const notFound = () => {
         <title>Page not found</title>
       </Helmet>
       <div style={{textAlign: "center", padding:"5vh 0", lineHeight: "1.5"}}>
-        <svg
-          width="256"
-          height="256"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M14.1254 13H10.1254V15H14.1254V13Z" fill="currentColor" />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M8.12537 13C9.22994 13 10.1254 12.1046 10.1254 11C10.1254 9.89543 9.22994 9 8.12537 9C7.0208 9 6.12537 9.89543 6.12537 11C6.12537 12.1046 7.0208 13 8.12537 13ZM8.12537 11.5C8.40151 11.5 8.62537 11.2761 8.62537 11C8.62537 10.7239 8.40151 10.5 8.12537 10.5C7.84922 10.5 7.62537 10.7239 7.62537 11C7.62537 11.2761 7.84922 11.5 8.12537 11.5Z"
-            fill="currentColor"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M18.1254 11C18.1254 12.1046 17.2299 13 16.1254 13C15.0208 13 14.1254 12.1046 14.1254 11C14.1254 9.89543 15.0208 9 16.1254 9C17.2299 9 18.1254 9.89543 18.1254 11ZM16.6254 11C16.6254 11.2761 16.4015 11.5 16.1254 11.5C15.8492 11.5 15.6254 11.2761 15.6254 11C15.6254 10.7239 15.8492 10.5 16.1254 10.5C16.4015 10.5 16.6254 10.7239 16.6254 11Z"
-            fill="currentColor"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M2.74884 14.6663C3.73056 16.6421 5.76939 18 8.12537 18H16.1254C18.5654 18 20.6652 16.5435 21.6029 14.4525C22.3722 13.9093 22.8746 13.0133 22.8746 12C22.8746 10.9867 22.3722 10.0907 21.6029 9.54753C20.6652 7.45651 18.5654 6 16.1254 6H8.12537C5.76939 6 3.73056 7.3579 2.74884 9.33375C1.78448 9.83263 1.12537 10.8393 1.12537 12C1.12537 13.1607 1.78448 14.1674 2.74884 14.6663ZM8.12537 8H16.1254C17.5088 8 18.7282 8.70234 19.4465 9.76991C19.7227 10.4593 19.8746 11.2119 19.8746 12C19.8746 12.7881 19.7227 13.5407 19.4465 14.2301C18.7282 15.2977 17.5088 16 16.1254 16H8.12537C5.91623 16 4.12537 14.2091 4.12537 12C4.12537 9.79086 5.91623 8 8.12537 8Z"
-            fill="currentColor"
-          />
-        </svg>
-        <p>Either you are in a wrong page or you have lost. Lets go back <Link to="/">home</Link> safely</p>
+    
+        <p> rezgauche.be is innovating, developing and practising inclusive structures that balance responsibility, resources and stakeholdership, with participation and decentralised forms of organisation.</p>
+        <p> Our gallery/atelier location is at Rue Theodore Verhaegen 154, Brussels, Belgium. The multi-purpose gallery/atelier is open for performance art, dance, artistic research, visual art, theatre, design, music production, audiovisual production and other forms of presenting and gathering around artistic practice. 
+       rezgauche.be* initiates a transition from a curated art gallery into a decentralised organisation.
+    For its realisation rezgauche.be follows a fourfold path
+    Work with a Smartbe Activity for durable artistic employment.
+    Start a cooperative practice to fund and share responsibility.
+    Make a DAO for governance by the participants.
+    Develop the gallery/atelier through Web3.
+    \n\n*rezgauche.be*\_openly accounts for developing its: location, employment, materials.\n\n*   
+      starting phase estimated *annual location costs: 8646 €   
+        development phase estimated total annual costs: 119 314 € </p>
+   <h3>roadmap</h3>
+   <p>
+      ### 2020
+
+      *   11
+          initial idea and desire for this experiment. [*read article*](/posts/debunking/)
+
+      <!---->
+
+      *   12
+          proposal for creating a sustainable economy for artist ecosystem. [*read article*](/posts/creating-a-sustainable-economy-for-the-artists-eco-system/)
+
+      ### 2021
+
+      *   1 - 4
+
+          *   build and launch website version 1.0
+
+          *   prepare AK exposition
+
+          *   prepare Motion workshop
+
+      <!---->
+
+      *   5 - 7
+
+          *   AK exposition
+
+          *   Motion [*workshop*](https://www.youtube.com/watch?v=XJnV41LQ7\_w)
+
+          *   Launch *open call*
+
+      <!---->
+
+      *   8 - 9
+
+          *   MotionDAO [*research*](https://www.dance-tech.net/profiles/blogs/crypto-in-motion-a-crypto-economy-for-dance-tech-creators)
+
+      <!---->
+
+      *   10 - 11
+
+          *   build website version 2.0
+
+          *   test case smart contract A.i.R
+
+      <!---->
+
+      *   12
+
+          *   launch website 2.0
+
+          *   cooperative stakeholders business plan A.i.R
+
+          *   contact partners
+
+      ### 2022
+
+      *   1 - 6
+
+          *   apply for/gather funds for starting phase
+
+          *   initiate seed funding round 1: founders
+
+          *   residencies, activities and events
+
+          *   beta phase smart contract
+
+      <!---->
+
+      *   7 - 8
+
+          *   compile year planning 2023
+
+          *   develop website version 3.0
+
+      <!---->
+
+      *   9 - 12
+
+          *   seed funding round 2: investors
+
+          *   residencies, activities and events
+
+          *   apply for funding development phase
+
+      ### 2023
+
+      *   1 - 2
+
+          *   extend into global networks
+
+          *   define public foundation (art, disparity, ecology)
+
+      *   2 - 6
+
+          *   residencies, activities and events
+
+          *   apply for EU funding Employment, A.i.R. and Activities
+
+          *   develop economy of solidarity
+
+      *   7 - 8
+
+          *   compile year planning 2024
+
+          *   apply for funding economy of solidarity
+
+          *   develop public foundation
+
+      *   9 - 12
+
+          *   launch website 3.0
+
+          *   alpha phase smart contract
+
+          *   residencies, activities and events
+
+          *   seed funding round 3: economy of solidarity
+
+          *   strategic real estate & ecological development
+
+      ### 2024
+
+      *   1 - 6
+
+          *   launch public foundation
+
+          *   residencies, activities and events
+
+      *   7 - 8
+
+          *   evaluation of 2020-2024 four year plan
+
+          *   compile year planning 2025
+
+      *   9 - 12
+
+          *   develop next roadmap
+          
+          
+          <Link to="/">home</Link> safely</p>
+          
+          
       </div>
     </Layout>
   )
