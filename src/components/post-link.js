c1ca658fb4b5a7b15dadfd30148d9149fcaf9ec5
rezgauche <94529557+rezgauche@users.noreply.github.com>
2022-01-25 23:42:06 +0100

    Update post-link.js

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -14,7 +14,6 @@ const PostLink = ({ post }) => (
           {post.frontmatter.title}
         </Link>
       </h2>
-      <div className="post-meta">{post.frontmatter.date}</div>
     </header>
   </article>
 )
