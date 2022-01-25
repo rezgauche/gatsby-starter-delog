0ff3c5595f08e84c90b6aee4b0e34c70d5faf78f
rezgauche <94529557+rezgauche@users.noreply.github.com>
2022-01-25 22:34:56 +0100

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+const ContactPage = ({
+  data: {
+    site
+  },
+}) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>About — {site.siteMetadata.title}</title>
+        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+      </Helmet>
+      <div className="two-grids -contact">
+        <div className="post-thumbnail" style={{backgroundImage: `url(), marginBottom: 0}}>
+          <h1 className="post-title">Contact</h1>
+          <p> mail@rezgauche.be &rarr;</p>
+   
+        </div>
+   
+      </div>
+    </Layout>
+  )
+}
+export default ContactPage
+export const pageQuery = graphql`
+  query ContactPageQuery{
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
