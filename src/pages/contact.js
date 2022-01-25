5d8a1da2198e6f872f408c833aa60af39c54189d
rezgauche <94529557+rezgauche@users.noreply.github.com>
2022-01-25 22:06:53 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,11 +17,37 @@ const ContactPage = ({
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('https://arweave.net/dn6z-GqVMI3psbX_EM4SsyeMENyI_MCTcaweuP61pR0')`, marginBottom: 0}}>
           <h1 className="post-title">Contact</h1>
-          <p>lets get in touch &rarr;</p>
-        </div>
-            <p> mail@rezgauche.be &rarr; +32 488 367 723" &rarr; Theodore Verhaegenstraat 154, Brussels 1060, Belgium &rarr; 
+          <p> mail@rezgauche.be &rarr;</p>
+          <p> +32 488 367 723 &rarr; Theodore Verhaegenstraat 154, Brussels 1060, Belgium &rarr; 
             administration: smartbe.be, Verenigde Producties vzw, activiteit nr. 06349 transdisciplinary art foundation, project rezgauche V.A.T. BE 0896.755.397 &rarr;
             opencollective.com/rezgauche </p>
+        </div>
+          
+      <!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7_dtp.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://rezgauche.us20.list-manage.com/subscribe/post?u=ceaf83d0eab081c40e7ef298c&amp;id=fee28ca32a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ceaf83d0eab081c40e7ef298c_fee28ca32a" tabindex="-1" value=""></div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                <p class="brandingLogo"><a href="http://eepurl.com/hS7u51" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
       </div>
     </Layout>
   )
