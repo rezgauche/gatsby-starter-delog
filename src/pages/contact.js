36913a1f6fc79ff4a38ba3cb6e3d69b05fad6302
rezgauche <94529557+rezgauche@users.noreply.github.com>
2022-01-25 21:57:02 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,11 +16,12 @@ const ContactPage = ({
       </Helmet>
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('https://arweave.net/dn6z-GqVMI3psbX_EM4SsyeMENyI_MCTcaweuP61pR0')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
-          <p>"let's get in touch" &rarr;</p>
+          <h1 className="post-title">Contact</h1>
+          <p>lets get in touch &rarr;</p>
         </div>
-            <p> "mail@rezgauche.be" &rarr;</p>
-             <p> "+32 488 367 723" &rarr;</p>
+            <p> mail@rezgauche.be &rarr; +32 488 367 723" &rarr; Theodore Verhaegenstraat 154, Brussels 1060, Belgium &rarr; 
+            administration: smartbe.be, Verenigde Producties vzw, activiteit nr. 06349 transdisciplinary art foundation, project rezgauche V.A.T. BE 0896.755.397 &rarr;
+            opencollective.com/rezgauche </p>
       </div>
     </Layout>
   )
