dd47d30db3c2552c5af6a79f853ff1b1717c60a0
rezgauche <94529557+rezgauche@users.noreply.github.com>
2022-01-25 21:36:40 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,7 +19,18 @@ const ContactPage = ({
           <h1 className="post-title">Get in Touch</h1>
           <p>"let's get in touch" &rarr;</p>
         </div>
-      
+            <p> [mail@rezgauche.be](mailto:mail@rezgauche.be)
+            +32 488 367 723
+            *address*
+            [Theodore Verhaegenstraat 154, rezgauche](https://goo.gl/maps/Cf1iwYui98ooRFa97),
+            B-1060 Brussels, Belgium
+            *administration*
+            [smartbe.be](https://smartbe.be/en/) Verenigde Producties vzw
+            activiteit nr. 06349
+            transdisciplinary art foundation,
+            project rezgauche.
+            V.A.T. BE 0896.755.397
+            [opencollective.com/rezgauche](https://opencollective.com/rezgauche)</p>
       </div>
     </Layout>
   )
